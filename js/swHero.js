f03e8d064fc9c886c35b65eaecc6bdd70dbd6357
fotia-a7 <kuto0406@gmail.com>
2026-09-08 16:49:31 +0900

    히어로 슬라이드 스와이퍼 내비게이션 버튼 비활성화, 자동재생 버튼 구현

--- js/swHero.js
+++ js/swHero.js
@@ -2,13 +2,28 @@ const mainVisualBg = document.querySelector(
   ".hero .main-visual .main-visual-bg",
 );
 // console.log(mainVisualBg);
+const heroBtns = document.querySelectorAll(".visual-inner button");
+// console.log(heroBtns);
+const playstopbtn = document.querySelector(".sw-hero .play-stop-btn");
+console.log(playstopbtn);
 
+// 히어로 슬라이드 버튼 호버 인터랙션
+heroBtns.forEach((btn) => {
+  btn.addEventListener("mouseenter", () => {
+    btn.querySelector(".off").style.opacity = 1;
+  });
+  btn.addEventListener("mouseout", () => {
+    btn.querySelector(".off").style.opacity = 0;
+  });
+});
+
+// 스와이퍼 설정
 const swHero = new Swiper(".sw-hero", {
   speed: 1000,
-  // autoplay: {
-  //   delay: 5000,
-  //   disableOnInteraction: false,
-  // },
+  autoplay: {
+    delay: 5000,
+    disableOnInteraction: false,
+  },
   pagination: {
     el: ".swiper-pagination",
     type: "fraction",
@@ -23,11 +38,29 @@ const swHero = new Swiper(".sw-hero", {
       // const activeSilde = swiper.activeIndex + 1;
       // console.log(activeSilde);
       // mainVisualBg.style.backgroundImage = `url(assets/images/slide_${activeSilde}.png)`;
-      const activeSilde = swiper.slide[swiper.activeindex];
-      // console.log(activeSilde);
-      const activeSildeimg = activeslide.querySelector(".slide-image img");
-      // console.log(activeSildeimg.src);
-      mainVisualBg.computedStyleMap.backgroundimage = `url(${activeSildeimg.src})`;
+      const activeSlide = swiper.slides[swiper.activeIndex];
+      // console.log(activeSlide);
+      const activeSlideImgUrl =
+        activeSlide.querySelector(".slide-image img").src;
+      // console.log(activeSlideImg.src);
+      mainVisualBg.style.backgroundImage = `url(${activeSlideImgUrl})`;
     },
   },
+});
+
+// 재생, 정지 버튼
+playstopbtn.addEventListener("click", () => {
+  // console.log(슬라이드 자동재생 정지);
+  swhero.autoplay.stop();
+  const isrunning = swhero.autoplay.running;
+  console.log(isrunning);
+
+  if (isrunning) {
+    swhero.autoplay.stop();
+    console.log("자동재생 정지");
+    playstopbtn.style.backgroundImage = `url(assets/icons/slide_play.png)`;
+  } else {
+    swhero.autoplay.start();
+    playstopbtn.style.backgroundImage = `url(assets/icons/slide_stop.png)`;
+  }
 });
